e114ca312a43d04d7e0ada9702255cca7bb398b6
Irina-coud <irinatobolich313@gmail.com>
2021-03-12 13:27:44 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverseNum(n) {
+    let total = 0;
+    n = Math.abs(n);
+
+    while (n) {
+        total = total * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return total;
 }
