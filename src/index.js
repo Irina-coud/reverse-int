982bc73e2ca1070c65416440b0ba3b242632d2a5
Irina-coud <irinatobolich313@gmail.com>
2021-07-07 10:42:00 +0300

    add new solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-module.exports = function reverseNum(n) {
-    let total = 0;
-    n = Math.abs(n);
+// module.exports = function reverseNum(n) {
+//     let total = 0;
+//     n = Math.abs(n);
 
-    while (n) {
-        total = total * 10 + n % 10;
-        n = Math.floor(n / 10);
-    }
-    return total;
+//     while (n) {
+//         total = total * 10 + n % 10;
+//         n = Math.floor(n / 10);
+//     }
+//     return total;
+// }
+
+module.exports = function reverseNum(num) {
+    num = Math.abs(num);
+    return +num.toString().split("").reverse().join("");
 }
